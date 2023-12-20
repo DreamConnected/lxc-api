db3dcc5ae78e1179a06e7420abb38b8eede66610
DreamConnected <109940850+DreamConnected@users.noreply.github.com>
2023-12-20 11:57:17 +0800

    Update main.go
    Add web terminal on 8001 port;
    Fixed shuffling of delete and add container and get container info functions.

--- main.go
+++ main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
+	"embed"
 	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
 	"os/exec"
 	"strings"
 	"time"
 
+	"github.com/creack/pty"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+	"github.com/olahol/melody"
 )
+
+//go:embed index.html node_modules/xterm/css/xterm.css node_modules/xterm/lib/xterm.js
+var content embed.FS
+var apiVersion = "b537c464"
 
 type VersionResponse struct {
 	Version string `json:"version"`
+}
+
+func GetAPIVersion(w http.ResponseWriter, r *http.Request) {
+	resp := &VersionResponse{
+		Version: apiVersion,
+	}
+
+	jsonData, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
 }
 
 type ContainerResponse struct {
@@ -48,16 +72,16 @@ type Response struct {
 }
 
 type ContainerCreateRequest struct {
-    Template     string `json:"template"`
-    ContainerName string `json:"container_name"`
-    ImageSource  string `json:"image_source"`
-    Distribution string `json:"distribution"`
-    Release      string `json:"release"`
-    Architecture string `json:"architecture"`
+	Template      string `json:"template"`
+	ContainerName string `json:"container_name"`
+	ImageSource   string `json:"image_source"`
+	Distribution  string `json:"distribution"`
+	Release       string `json:"release"`
+	Architecture  string `json:"architecture"`
 }
 
 type ContainerDestroyRequest struct {
-    ContainerName string `json:"del_container"`
+	ContainerName string `json:"del_container"`
 }
 
 func GetVersion(w http.ResponseWriter, r *http.Request) {
@@ -69,8 +93,10 @@ func GetVersion(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	version := strings.TrimSuffix(string(output), "\n")
+
 	resp := &VersionResponse{
-		Version: string(output),
+		Version: version,
 	}
 
 	jsonData, err := json.Marshal(resp)
@@ -307,97 +333,127 @@ func UnfreezeContainer(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateContainer(w http.ResponseWriter, r *http.Request) {
-    var request ContainerCreateRequest
-    err := json.NewDecoder(r.Body).Decode(&request)
-    if err != nil {
-        resp := &Response{
-            StatusCode: http.StatusBadRequest,
-            Message:    "Invalid request format",
-        }
-        jsonData, _ := json.Marshal(resp)
-        w.Header().Set("Content-Type", "application/json")
-        w.WriteHeader(http.StatusBadRequest)
-        w.Write(jsonData)
-        return
-    }
+	var request ContainerCreateRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		resp := &Response{
+			StatusCode: http.StatusBadRequest,
+			Message:    "Invalid request format",
+		}
+		jsonData, _ := json.Marshal(resp)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(jsonData)
+		return
+	}
 
-    cmd := exec.Command("lxc-create",
-        "-t", request.Template,
-        "-n", request.ContainerName,
-        "--",
-        "--server", request.ImageSource,
-        "--dist", request.Distribution,
-        "--release", request.Release,
-        "--arch", request.Architecture,
-    )
+	cmd := exec.Command("lxc-create",
+		"-t", request.Template,
+		"-n", request.ContainerName,
+		"--",
+		"--server", request.ImageSource,
+		"--dist", request.Distribution,
+		"--release", request.Release,
+		"--arch", request.Architecture,
+	)
 
-    err = cmd.Run()
-    if err != nil {
-        resp := &Response{
-            StatusCode: http.StatusInternalServerError,
-            Message:    err.Error(),
-        }
-        jsonData, _ := json.Marshal(resp)
-        w.Header().Set("Content-Type", "application/json")
-        w.WriteHeader(http.StatusInternalServerError)
-        w.Write(jsonData)
-        return
-    }
+	err = cmd.Run()
+	if err != nil {
+		resp := &Response{
+			StatusCode: http.StatusInternalServerError,
+			Message:    err.Error(),
+		}
+		jsonData, _ := json.Marshal(resp)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(jsonData)
+		return
+	}
 
-    resp := &Response{
-        StatusCode: http.StatusOK,
-        Message:    "Container created successfully",
-    }
-    jsonData, _ := json.Marshal(resp)
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    w.Write(jsonData)
+	resp := &Response{
+		StatusCode: http.StatusOK,
+		Message:    "Container created successfully",
+	}
+	jsonData, _ := json.Marshal(resp)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonData)
 }
 
 func DestroyContainer(w http.ResponseWriter, r *http.Request) {
-    var request ContainerDestroyRequest
-    err := json.NewDecoder(r.Body).Decode(&request)
-    if err != nil {
-        resp := &Response{
-            StatusCode: http.StatusBadRequest,
-            Message:    "Invalid request format",
-        }
-        jsonData, _ := json.Marshal(resp)
-        w.Header().Set("Content-Type", "application/json")
-        w.WriteHeader(http.StatusBadRequest)
-        w.Write(jsonData)
-        return
-    }
+	var request ContainerDestroyRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		resp := &Response{
+			StatusCode: http.StatusBadRequest,
+			Message:    "Invalid request format",
+		}
+		jsonData, _ := json.Marshal(resp)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(jsonData)
+		return
+	}
 
-    cmd := exec.Command("lxc-destroy", "-n", request.ContainerName)
+	cmd := exec.Command("lxc-destroy", "-n", request.ContainerName)
 
-    err = cmd.Run()
-    if err != nil {
-        resp := &Response{
-            StatusCode: http.StatusInternalServerError,
-            Message:    err.Error(),
-        }
-        jsonData, _ := json.Marshal(resp)
-        w.Header().Set("Content-Type", "application/json")
-        w.WriteHeader(http.StatusInternalServerError)
-        w.Write(jsonData)
-        return
-    }
+	err = cmd.Run()
+	if err != nil {
+		resp := &Response{
+			StatusCode: http.StatusInternalServerError,
+			Message:    err.Error(),
+		}
+		jsonData, _ := json.Marshal(resp)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(jsonData)
+		return
+	}
 
-    resp := &Response{
-        StatusCode: http.StatusOK,
-        Message:    "Container destroyed successfully",
-    }
-    jsonData, _ := json.Marshal(resp)
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    w.Write(jsonData)
+	resp := &Response{
+		StatusCode: http.StatusOK,
+		Message:    "Container destroyed successfully",
+	}
+	jsonData, _ := json.Marshal(resp)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonData)
+}
+
+var templates *template.Template
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	upgrader := websocket.Upgrader{}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade error:", err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Read error:", err)
+			break
+		}
+
+		log.Println("Received message:", string(message))
+
+		if err := conn.WriteMessage(messageType, message); err != nil {
+			log.Println("Write error:", err)
+			break
+		}
+	}
 }
 
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/version", GetVersion).Methods("GET")
+
+	r.HandleFunc("/apiversion", GetAPIVersion).Methods("GET")
 
 	r.HandleFunc("/containers", GetContainers).Methods("GET")
 
@@ -408,12 +464,42 @@ func main() {
 	r.HandleFunc("/container/{name}/stop", StopContainer).Methods("POST")
 
 	r.HandleFunc("/container/{name}/freeze", FreezeContainer).Methods("POST")
-	
+
 	r.HandleFunc("/container/{name}/unfreeze", UnfreezeContainer).Methods("POST")
 
-	r.HandleFunc("/container/create", CreateContainer).Methods("POST")
+	r.HandleFunc("/add/container", CreateContainer).Methods("POST")
 
-	r.HandleFunc("/container/destroy", DestroyContainer).Methods("POST")
+	r.HandleFunc("/del/container", DestroyContainer).Methods("POST")
+
+	c := exec.Command("sh")
+	f, err := pty.Start(c)
+	if err != nil {
+		panic(err)
+	}
+
+	m := melody.New()
+
+	go func() {
+		for {
+			buf := make([]byte, 1024)
+			read, err := f.Read(buf)
+			if err != nil {
+				return
+			}
+			m.Broadcast(buf[:read])
+		}
+	}()
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		f.Write(msg)
+	})
+
+	http.HandleFunc("/webterminal", func(w http.ResponseWriter, r *http.Request) {
+		m.HandleRequest(w, r)
+	})
+
+	fs := http.FileServer(http.FS(content))
+	http.Handle("/", http.StripPrefix("/", fs))
 
 	srv := &http.Server{
 		Handler:      r,
@@ -422,5 +508,9 @@ func main() {
 		ReadTimeout:  15 * time.Second,
 	}
 
-	log.Fatal(srv.ListenAndServe())
+	go func() {
+		log.Fatal(srv.ListenAndServeTLS("/data/lxc/lxc-api.crt", "/data/lxc/lxc-api.key"))
+	}()
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:8001", nil))
 }
